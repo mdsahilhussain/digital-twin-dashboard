c04f5e3fc85233ec9edd2d1c46d83c5e96e3e759
mdsahilhussain <mdsahil.hussain7@gmail.com>
2026-02-26 13:15:11 +0530

    Add GrowthSection component and update KPISection styles

--- app/components/KPISection.tsx
+++ app/components/KPISection.tsx
@@ -3,7 +3,7 @@
 import { useStore } from "@/lib/store"
 
 
-export default function KPISection() {
+const  KPISection = () => {
   const { data, toggleVerification } = useStore()
 
   const jobs = data.metrics.total_jobs.value
@@ -12,11 +12,13 @@ export default function KPISection() {
     <div className="grid grid-cols-3 gap-6 mt-10">
       <div
         onClick={toggleVerification}
-        className="bg-gray-800 p-6 rounded-xl cursor-pointer hover:bg-gray-700 transition"
+        className="bg-gray-800 p-2.5 md:p-6 rounded-xl cursor-pointer hover:bg-gray-700 transition"
       >
-        <p className="text-gray-400">Total Jobs</p>
-        <h2 className="text-3xl font-bold mt-2">{jobs.toLocaleString()}</h2>
+        <p className="text-gray-400 text-xs md:text-base">Total Jobs</p>
+        <h2 className="text-2xl md:text-3xl font-bold mt-1 md:mt-2">{jobs.toLocaleString()}</h2>
       </div>
     </div>
   )
 }
+
+export default KPISection
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import KPISection from "./components/KPISection";
+import GrowthSection from "@/components/GrowthSection";
+import KPISection from "@/components/KPISection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-950 text-white p-10">
-      <h1 className="text-3xl font-bold">
-        Digital Twin Dashboard
-      </h1>
-      <KPISection/>
+    <main className="min-h-screen bg-gray-950 text-white p-10 max-sm:px-4">
+      <h1 className="text-3xl font-bold">Digital Twin Dashboard</h1>
+      <KPISection />
+      <GrowthSection />
     </main>
-  )
+  );
 }
